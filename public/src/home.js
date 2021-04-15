5f50df16485f268358c0705449265bb589f10699
Mason Bruning <jmbrunin@oakland.edu>
2021-04-15 15:32:05 -0400

    Update home.js

--- public/src/home.js
+++ public/src/home.js
@@ -1,13 +1,22 @@
 function totalCount(array){
   //return the total number of books.
-  /*
-  let totalCount = array.reduce(function(prev, cur) {
-    return prev + cur.totalCount;
-  }, 0);
-  return totalCount;
-  */
+  //Takes in an array, and just returns an integer representing it's length.
  return array.length;
 }
+
+//Helper function
+//Even though it is const, it will change its value each time when called.
+//Takes in an object
+const sortObjectByValues = (obj) => {
+    const keys = Object.keys(obj);
+  return keys.sort((keyA, keyB) => {
+    if (obj[keyA] > obj[keyB]) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+};
 
 function getTotalBooksCount(books) {
   //returns the number of book objects.
@@ -23,39 +32,55 @@ function getTotalAccountsCount(accounts) {
 }
 
 function getBooksBorrowedCount(books) {
-  //It returns a number that represents the number of books 
-  //_that are currently checked out of the library.
-  //This number can be found by looking at the first transaction in the `borrows` key of each book.
-  //If the transaction says the book has not been returned (i.e. `returned: false`), 
-  //the book has been borrowed.
-  const result=books.every((book) => book.borrows[returned] === false);
-  return result.length;
+  /*
+  It returns a number that represents the number of books 
+  _that are currently checked out of the library.
+  This number can be found by looking at the first transaction in the `borrows` key of each book.
+  If the transaction says the book has not been returned (i.e. `returned: false`), 
+  the book has been borrowed.
+  */
+  //const result=books.every((book) => book.borrows[returned] === false);
+  //return result.length.
+  const bookCheckedOut = books.filter((book) => {
+    return book.borrows[0].returned === false;
+  });
+  return bookCheckedOut.length;
 }
 
+//Takes in an array of objects
 function getMostCommonGenres(books) {
-    //returns an array of 5 or less objects representing the most common borrowed genres.
-  //Ordered most common to least.
-  //Each object in the return array has 2 keys:
-  //Name and Count
-  /*
-   [
-    { name: "Nonfiction", count: 9 },
-    { name: "Historical Fiction", count: 7 },
-    { name: "Thriller", count: 7 },
-    ...
-  ]
-  */
-  //Must go through each book to count how many times it's been checked out.
-  //While counting the array length, must keep track of genre.
-  //After compiling, should add same genres together.
-  //Order highest to lowest.
-  //Return map of this genre to count
+  /*returns an array of 5 or less objects representing the most common borrowed genres.
+  Ordered most common to least. Each object in the return array has 2 keys:
+  Name and Count
+  
+  Must go through each book to count how many times it's been checked out.
+  While counting the array length, must keep track of genre.
+  After compiling, should add same genres together.
+  Order highest to lowest.
+  Return map of this genre to count
 
-  //Have it count how many times each book has been borrowed.
-  //Map book to genre
-  //Combine sums of respected genres
-  //Order highest to lowest (using sort)
-  //Return Map of top 5
+  Have it count how many times each book has been borrowed.
+  Map book to genre
+  Combine sums of respected genres
+  Order highest to lowest (using sort)
+  Return Map of top 5
+  */
+  
+  const allGenres = books.reduce((acc, {genre}) => {
+   if (acc[genre]) {
+     acc[genre] += 1;
+   }
+   else {
+     acc[genre] = 1;
+   }
+   return acc;
+  }, {});
+  
+  const sortedGenres = sortObjectByValues(allGenres);
+
+  let finalSortedArr = sortedGenres.map((item) => ({name: item, count: allGenres[item]})).slice(0,5);
+ 
+  return finalSortedArr;
 }
 
 function getMostPopularBooks(books) {
@@ -75,7 +100,16 @@ function getMostPopularBooks(books) {
   Map count to title.
   Order books (using sort) from most borrowed to least.
   Return top 5.
-  */
+  */  
+  let popularBooks = books.reduce((acc, {title}, counter) => {
+    acc[title] = books[counter].borrows.length;
+    counter++;
+    return acc;
+  }, {});
+  let sortedBooks = sortObjectByValues(popularBooks);
+  let formattedBooksArr = sortedBooks.map((item) => ({name: item, count: popularBooks[item]})).slice(0,5);
+
+ return formattedBooksArr;
 }
 
 function getMostPopularAuthors(books, authors) {
@@ -98,6 +132,29 @@ function getMostPopularAuthors(books, authors) {
   Order array (using sort) from most borrowed to least.
   Return top 5.
   */
+  //allAuthorsById is an array that has a counter, 
+    const allAuthorsById = books.reduce((acc, {authorId}, counter, authorNumberToName) => {
+      for (let i = 0; i < authors.length; i++) {
+      
+        if (books[counter].authorId === authors[i].id) {
+        authorNumberToName = Object.values(authors[i].name).join(' ');
+        break;
+      }
+    }
+    if (acc[authorNumberToName]) { 
+      acc[authorNumberToName] += books[counter].borrows.length;
+    } else {
+      acc[authorNumberToName] = books[counter].borrows.length;
+    }
+    counter++
+    return acc;
+  }, {});
+  
+  const sortedAuthors = sortObjectByValues(allAuthorsById);
+  
+  let formattedAuthorsArr = sortedAuthors.map((item) => ({name: item, count: allAuthorsById[item]})).slice(0,5);
+  
+  return formattedAuthorsArr;
 }
 
 module.exports = {
