04d84b461d5b05fc830d3e0b561f739dc7fc53d0
Mason Bruning <jmbrunin@oakland.edu>
2021-04-15 15:31:44 -0400

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,13 +1,20 @@
 function findAuthorById(authors, id) {
    //returns the author object matching the ID.
-   let foundAuthor = authors.id.find((author) => author.id === id);
-   return foundAuthor;
+  let foundAuthor = authors.find((author) => author.id === id);
+  return foundAuthor;
 }
 
 function findBookById(books, id) {
   //return the book object matching that ID.
-  let foundBook = books.id.find((book) => book.id === id);
+  let foundBook = books.find((book) => book.id === id);
   return foundBook;
+}
+
+function booksIsCheckedOutStatus(booksArray, boolean){
+  return booksArray.filter((item) => {
+    return item.borrows[0].returned == boolean
+  });
+  //return booksArray;
 }
 
 function partitionBooksByBorrowedStatus(books) {
@@ -16,14 +23,10 @@ function partitionBooksByBorrowedStatus(books) {
   //The second array is a list of books that have been loaned out and returned.
   //You can check for the return status by looking at the first transaction in the `borrows` array.
 
-  /*
-  Find and add each book that is borrowed.
-  Find and add each book that isn't borrowed.
-  */
-  let returnedBooks = books.borrows.filter((book) => book.borrow.returned === false);
-  let borrowedBooks = books.borrows.filter((book) => book.borrows.returned === true);
-
-  //combine and return arrays.
+  let borrowedBooks = booksIsCheckedOutStatus(books,false);
+  let returnedBooks = booksIsCheckedOutStatus(books,true);
+  let allBooks=[borrowedBooks, returnedBooks];
+  return allBooks;
 }
 
 function getBorrowersForBook(book, accounts) {
@@ -34,7 +37,24 @@ function getBorrowersForBook(book, accounts) {
   Create a loop to go through each book to push the users information into a new array.
   Add the books 'returned' key at end.
   return array.
-  */
+  */  
+ let {borrows} = book;//destructs the book object to just the borrows object
+  //for every borrow in the borrows object
+  let result = borrows.map((borrow) => {
+    //Creates an array of accounts that has borrow id match the account id.
+    let accountMatches = accounts.filter((account) => {
+      return account.id === borrow.id;
+    });
+    //Remove the extra account id, and return the destructed array.
+    let [acountsWithoutIds] = accountMatches.map((account) => {
+      delete account.id;
+      return account;
+    });
+    //Add the borrow's object keys and values with the account object key+values
+    let resultingBorrow = {...borrow, ...acountsWithoutIds};
+    return resultingBorrow;
+  })
+  return result.slice(0 ,10)
 }
 
 /*
